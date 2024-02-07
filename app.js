1ba17ccd942123e52a2f1b063cd89d2aac965032
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2024-02-07 05:33:14 -0500

    setup morgan for DRUD logging

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
+const morgan = require('morgan')
 const connectDB = require('./config/db')
 
 
 //environtment variables
 require('dotenv').config()
 
+
+//logging CRUD requests
+app.use(morgan('dev'))
+
+//connecting to DB
 connectDB()
 const PORT = process.env.PORT || 2641
 

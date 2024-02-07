63b74809b79cc14d532bdd3656cbeb59582a0eb4
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2024-02-07 05:55:20 -0500

    handlebars setup

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
 
 
 //environtment variables
 require('dotenv').config()
 
+//connecting to DB
+connectDB()
+
+//body parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 //logging CRUD requests
 app.use(morgan('dev'))
 
-//connecting to DB
-connectDB()
+//handlebars helpers
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: '.hbs'
+})
+
+//handlebars
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
+//static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
 const PORT = process.env.PORT || 2641
 
 app.listen(process.env.PORT, ()=>{

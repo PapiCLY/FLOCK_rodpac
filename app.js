ef87b9171b212c714c7bd33ca838d1331f95f4a3
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2024-02-07 05:19:28 -0500

    DB cluster setup

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const dotenv = require('dotenv')
 //environtment variables
 require('dotenv').config()
 
+const PORT = process.env.PORT || 2641
+
 app.listen(process.env.PORT, ()=>{
     console.log(`Server running on port ${process.env.PORT}`)
 })

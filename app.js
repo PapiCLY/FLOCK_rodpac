049e5ae8fc72249d4b1681d3af22520cd9ed4e3f
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2024-02-07 05:25:12 -0500

    connected to DB

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 
 
 //environtment variables
 require('dotenv').config()
 
+connectDB()
 const PORT = process.env.PORT || 2641
 
 app.listen(process.env.PORT, ()=>{
